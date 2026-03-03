34b2138497fc18efbb3afdd8aeebfee8c9c37d90
robdgs <107399627+robdgs@users.noreply.github.com>
2026-03-03 12:52:43 +0100

    deployment fix

--- src/components/Benefits/BenefitBullet.tsx
+++ src/components/Benefits/BenefitBullet.tsx
@@ -1,13 +1,11 @@
 import { motion } from "framer-motion"
 
 import { IBenefitBullet } from "@/types"
-import { childVariants } from "./BenefitSection"
 
 const BenefitBullet: React.FC<IBenefitBullet> = ({ title, description, icon }: IBenefitBullet) => {
     return (
         <motion.div
             className="flex flex-col items-center mt-8 gap-3 lg:gap-5 lg:flex-row lg:items-start"
-            variants={childVariants}
         >
             <div className="flex justify-center mx-auto lg:mx-0 flex-shrink-0 mt-3 w-fit">
         {icon}
